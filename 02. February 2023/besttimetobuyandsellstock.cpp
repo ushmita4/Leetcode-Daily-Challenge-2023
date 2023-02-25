7b405c9d0f63a3aa9ffb3f701368d33f2b4f07a4
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2023-02-25 21:09:05 +0530

    Create besttimetobuyandsellstock.cpp

---
+++ 02. February 2023/besttimetobuyandsellstock.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy=prices[0],maxProfit=0;
+        int n=prices.size();
+        for(int i=1;i<n;i++){
+            if(prices[i]<buy)
+                buy=prices[i];
+            else if((prices[i]-buy)>maxProfit)
+                maxProfit=prices[i]-buy;
+        }
+        return maxProfit;
+    }
+};
