6d4227e02b2410549e31c3ad68286139047e1f14
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2023-03-11 12:47:55 +0530

    Create convertsortedlisttobinarysearch.cpp

---
+++ 03. March/convertsortedlisttobinarysearch.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+   TreeNode* func(ListNode *head,int n)
+   {
+          if(n<=0)
+          return nullptr;
+          if(n==1)
+          return new TreeNode(head->val);
+
+          int mid=n/2;
+          ListNode *temp=head;
+          while(mid--){
+              temp=temp->next;
+          }
+           TreeNode*ans=new TreeNode(temp->val,func(head,n/2),func(temp->next,n-n/2-1));
+		
+        return ans;
+   }
+    TreeNode* sortedListToBST(ListNode* head) {
+        int n=0;
+      if(!head){
+          return nullptr;
+      }
+     ListNode *temp=head;
+     while(temp){
+         n++;
+         temp=temp->next;
+     }
+     return func(head,n);
+    }
+};
