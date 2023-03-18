cf6c5b3c9f1867922be62f009a52ce2185141ca4
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2023-03-18 22:10:02 +0530

    Create distributemoneytomaximumchildren.cpp

---
+++ 03. March/distributemoneytomaximumchildren.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int distMoney(int money, int children) {
+        money=money-children;
+        if(money<0)
+        return -1;
+        else{
+            if(money/7==children && money%7==0)
+            return children;
+            if (money / 7 == children - 1 && money % 7 == 3)
+            return children - 2;
+            return min(children - 1, money / 7);
+        }
+    }
+};
