2a4673e4982d8d1f39c123a3294396191991ed54
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2023-05-15 23:04:05 +0530

    Create swappingnodesinalinkedlist.cpp

---
+++ 05. April 2023/swappingnodesinalinkedlist.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+          ListNode *left = head, *right = head;
+    for(int i = 1; i < k; i++)
+        left = left->next;
+    ListNode *curr = left;
+    while(curr->next != nullptr){
+        curr = curr->next;
+        right = right->next;
+    }
+    int t = left->val;
+    left->val = right->val;
+    right->val = t;
+    return head;
+    }
+};
