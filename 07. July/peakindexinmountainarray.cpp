6854b28e5a55364181507cb077046ab95e9b3ca2
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2023-07-25 20:11:45 +0530

    Create peakindexinmountainarray.cpp

---
+++ 07. July/peakindexinmountainarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int left=0,right=arr.size()-1,mid;
+        while(left<right){
+             mid=left+(right-left)/2;
+             if(arr[mid]>arr[mid+1])
+             right=mid;
+             else
+             left=mid+1;
+        }
+        return left;
+    }
+};
