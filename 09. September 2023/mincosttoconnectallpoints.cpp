06044941efa25c0a5104fdeb244cf9af0de0f2c5
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2023-09-15 22:46:02 +0530

    Create mincosttoconnectallpoints.cpp

---
+++ 09. September 2023/mincosttoconnectallpoints.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    using int2=pair<int, int>;
+    int V;
+    int L1(vector<int>& P, vector<int>& Q){
+        return abs(P[0]-Q[0])+abs(P[1]-Q[1]);
+    }
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        V=points.size();
+        priority_queue<int2, vector<int2>, greater<int2>> pq;
+        pq.push({0, 0});
+        vector<bool> visited(V, 0);
+        vector<int> minWt(V, INT_MAX);
+        int ans = 0;
+        int n = 0;
+        while (!pq.empty()) {
+            auto [w, i] = pq.top();
+            pq.pop();
+            if (visited[i]) continue; // Skip visited 
+            visited[i]=1;
+            ans+= w;
+            n++;
+            if (n == V) return ans;
+            for (int j=0; j<V; j++){
+                if (visited[j]) continue;
+                int&& wt=L1(points[i], points[j]);
+                if (wt<minWt[j]){
+                    minWt[j]=wt;
+                    pq.push({wt, j});//push to pq only when wt<minWt[j]
+                }            
+            }
+        }
+        return -1;
+    }
+};
