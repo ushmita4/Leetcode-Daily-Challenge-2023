63133800f6e0387a251066657cf6c0cac7d05cbb
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2023-10-01 21:45:01 +0530

    Create reversewordsinastringIII.cpp

---
+++ 10. October 2023/reversewordsinastringIII.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        vector<char> t;
+        string ans="";
+        for (char c : s){
+            if (c==' '){//All the words in s are separated by a single space.
+                ans+=string(t.rbegin(), t.rend())+' ';
+                t.clear();
+            }
+            else t.push_back(c);
+       }
+       ans+=string(t.rbegin(), t.rend());
+       return ans;
+    }
+};
