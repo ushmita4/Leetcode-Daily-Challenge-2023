741943eadb0aaff791ee712642a945a141dbdd64
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2023-10-02 20:54:07 +0530

    Create removecoloredpiecesifbothneighboursarethesamecolor.cpp

---
+++ 10. October 2023/removecoloredpiecesifbothneighboursarethesamecolor.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool winnerOfGame(string colors) {
+        int flag=0;
+        int count1=0,c2=0;
+        for(int i=1;i<colors.size()-1;i++){
+            if(colors[i]=='A' && colors[i-1]=='A' && colors[i+1]=='A' ){
+               count1++;
+            }
+            else if(colors[i]=='B' && colors[i-1]=='B' && colors[i+1]=='B' ){
+                c2++;
+            }      
+        }
+        return count1>c2;
+    }
+};
