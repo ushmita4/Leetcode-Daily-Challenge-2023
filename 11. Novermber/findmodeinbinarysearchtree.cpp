623f3cc467e7ae46e49307602baa2bf10bdfd511
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2023-11-01 22:28:42 +0530

    Create findmodeinbinarysearchtree.cpp

---
+++ 11. Novermber/findmodeinbinarysearchtree.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> findMode(TreeNode* root) {
+        unordered_map<int, int> counter;
+        queue<TreeNode*> queue;
+        queue.push(root);
+        
+        while (!queue.empty()) {
+            TreeNode* node = queue.front();
+            queue.pop();
+
+            counter[node->val]++;
+            if (node->left != nullptr) {
+                queue.push(node->left);
+            }
+            if (node->right != nullptr) {
+                queue.push(node->right);
+            }
+        }
+        
+        int maxFreq = 0;
+        for (auto& [key, val] : counter) {
+            maxFreq = max(maxFreq, val);
+        }
+        
+        vector<int> ans;
+        for (auto& [key, val] : counter) {
+            if (val == maxFreq) {
+                ans.push_back(key);
+            } 
+        }
+        
+        return ans;
+    }
+}
