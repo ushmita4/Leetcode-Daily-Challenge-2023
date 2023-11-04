c0c33b5e3694b32619127ddd1a4116529600fa82
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2023-11-04 22:06:10 +0530

    Create lastmomentbeforeallantsfalloutofplank.cpp

---
+++ 11. Novermber/lastmomentbeforeallantsfalloutofplank.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        int ans=0;
+        for(int i=0;i<left.size();i++)
+        ans=max(ans,left[i]);
+
+        for(int i=0;i<right.size();i++)
+        ans=max(ans,abs(n-right[i]));
+
+        return ans;
+    }
+};
