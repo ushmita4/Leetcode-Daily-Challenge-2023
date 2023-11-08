b7270a97fd50f5df1409d3045e5047018a03c1a5
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2023-11-08 23:11:04 +0530

    Create determineifacellisreachableatagiventime.cpp

---
+++ 11. Novermber/determineifacellisreachableatagiventime.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+        if(sx==fx && sy==fy){
+            if(t==1)
+                return false;
+            else
+                return true;
+        }
+        int maxi=max(abs(sx-fx),abs(sy-fy));
+        if(maxi<=t)
+            return true;
+        else
+            return false;
+    }
+};
